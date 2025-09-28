b80e7602cc011754c6ef30a88007cc492d7cf313
Chan Jun Shern <JunShern@users.noreply.github.com>
2025-09-28 15:06:55 +0100

    Add I'm Feeling Lucky links for Ultimate Guitar chords

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
           
           const [artist, song, chordsLink, tabsLink, sheetLink] = row;
           const li = document.createElement('li');
-          
+
           let links = [];
           if (chordsLink) links.push(`[<a href="${chordsLink}">chords</a>]`);
           if (tabsLink) links.push(`[<a href="${tabsLink}">tabs</a>]`);
           if (sheetLink) links.push(`[<a href="${sheetLink}">sheet</a>]`);
-          
+          const luckyQuery = `${artist} ${song} chords site:ultimate-guitar.com`;
+          const luckyLink = `https://www.google.com/search?btnI=1&q=${encodeURIComponent(luckyQuery)}`;
+          links.push(`[<a href="${luckyLink}">I'm Feeling Lucky</a>]`);
+
           li.innerHTML = `${artist} - ${song} ${links.join(' ')}`;
           musicList.appendChild(li);
         }
